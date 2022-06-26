8b8b5465c16553955cf6f88e46165215a9e11610
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2022-06-26 12:47:37 +0000

    make it better

--- start.sh
+++ start.sh
@@ -6,3 +6,5 @@ export ENVIRONMENT=local
 
 mvn spring-boot:run
 #mvn package
+
+export url = postgres://csrnkhoervazrd:98d0881059afa2e083ba308b4bcdb62f2c87de1f86f89b283ebfa35597e7dd9a@ec2-44-206-89-185.compute-1.amazonaws.com:5432/d27sn2esjb5hs5
