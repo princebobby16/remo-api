ae28c2490398861424760cf8590f60f2531b6be7
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2022-06-26 18:03:56 +0000

    make it better

--- src/main/java/com/example/momorecorder/response/HealthCheckResponse.java
+++ src/main/java/com/example/momorecorder/response/HealthCheckResponse.java
@@ -10,7 +10,7 @@ public class HealthCheckResponse {
     public HashMap<String, Object> returnHealthStatus() {
         HashMap<String, Object> response = new HashMap<>();
         response.put("email", "eng.admin@greyparrot.io");
-        response.put("system", "Field Service Report Sender");
+        response.put("system", "Mobile Money Transactions Recorder(REMO)");
         response.put("version", "0.0.1"); // TODO: fetch this from the properties file
         response.put("environment", "LOCAL");
         response.put("status", "OK");
