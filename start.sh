daeef59d3aba8a94244e3d4e09f229e6edac1855
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2022-06-26 12:57:07 +0000

    make it better

--- start.sh
+++ start.sh
@@ -7,4 +7,7 @@ export ENVIRONMENT=local
 mvn spring-boot:run
 #mvn package
 
-export url = postgres://csrnkhoervazrd:98d0881059afa2e083ba308b4bcdb62f2c87de1f86f89b283ebfa35597e7dd9a@ec2-44-206-89-185.compute-1.amazonaws.com:5432/d27sn2esjb5hs5
+
+#export url=postgres://username:password@url:port/db_name
+#export url=postgres://csrnkhoervazrd:98d0881059afa2e083ba308b4bcdb62f2c87de1f86f89b283ebfa35597e7dd9a@ec2-44-206-89-185.compute-1.amazonaws.com:5432/d27sn2esjb5hs5
+#export dbUrl="jdbc:postgresql://ec2-44-206-89-185.compute-1.amazonaws.com:5432/d27sn2esjb5hs5"
