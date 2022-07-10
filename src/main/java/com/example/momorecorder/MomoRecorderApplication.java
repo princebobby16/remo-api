40bcd51480fdd312b1ab2d991d5d4d464a44bc90
PRINCE BOBBY <pbobby001@st.ug.edu.gh>
2022-07-10 17:13:23 +0000

    drop schema on startup

--- src/main/java/com/example/momorecorder/MomoRecorderApplication.java
+++ src/main/java/com/example/momorecorder/MomoRecorderApplication.java
@@ -1,7 +1,10 @@
 package com.example.momorecorder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
 public class MomoRecorderApplication {
